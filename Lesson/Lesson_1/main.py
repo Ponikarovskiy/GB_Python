538cdd042b246554645e21f6e986faa14c8f3056
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-03-11 17:35:31 +0300

    Lesson_1

---
+++ Lesson/Lesson_1/main.py
@@ -0,0 +1,37 @@
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура, это же МАША')
+# elif username == 'Марина':
+#     print('Я так ждала Вас, Марина!')
+# elif username == 'Ильнар':
+#     print('Ильнар - топ')
+# else:
+#     print('Привет, ', username)
+
+
+# i = 0
+# while i < 5:
+#     if i == 4:
+#         break
+#     i = i + 1
+# else:
+#     print('Пожалуй')
+#     print('хватит )')
+# print(i)
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:
+#         flag = False
+#         print(i)
+#     elif i > n // 2:
+#         print(n)
+#         flag = False
+#     i += 1
+
+a = 'qwerty'
+
+for i in a:
+    print(i)
